5a5e3d5e8b734f9f56818b698ef43ecbca422634
tjd <txomin.jimenez.dev@gmail.com>
2016-03-06 17:18:57 +0100

    Added gitignore and cleaned repo from unnecesary files

--- build/Release/.deps/Release/obj.target/uDesCrypto/src/UDES.o.d
+++
@@ -1,8 +0,0 @@
-cmd_Release/obj.target/uDesCrypto/src/UDES.o := c++ '-DNODE_GYP_MODULE_NAME=uDesCrypto' '-D_DARWIN_USE_64_BIT_INODE=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/Users/tjd/.node-gyp/0.12.9/include/node -I/Users/tjd/.node-gyp/0.12.9/src -I/Users/tjd/.node-gyp/0.12.9/deps/uv/include -I/Users/tjd/.node-gyp/0.12.9/deps/v8/include -I../node_modules/nan  -Os -gdwarf-2 -mmacosx-version-min=10.7 -arch x86_64 -Wall -Wendif-labels -W -Wno-unused-parameter -fno-rtti -fno-exceptions -fno-threadsafe-statics -std=c++11 -stdlib=libc++ -DTARGET_OS_OPENSTEP=1 -MMD -MF ./Release/.deps/Release/obj.target/uDesCrypto/src/UDES.o.d.raw   -c -o Release/obj.target/uDesCrypto/src/UDES.o ../src/UDES.cc
-Release/obj.target/uDesCrypto/src/UDES.o: ../src/UDES.cc \
-  ../src/KDefinitions.h ../src/UDES.h ../src/UByteSex.h ../src/UTInt64.h
-../src/UDES.cc:
-../src/KDefinitions.h:
-../src/UDES.h:
-../src/UByteSex.h:
-../src/UTInt64.h:
--- build/Release/.deps/Release/obj.target/uDesCrypto/src/UUTF16CStr.o.d
+++
@@ -1,9 +0,0 @@
-cmd_Release/obj.target/uDesCrypto/src/UUTF16CStr.o := c++ '-DNODE_GYP_MODULE_NAME=uDesCrypto' '-D_DARWIN_USE_64_BIT_INODE=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/Users/tjd/.node-gyp/0.12.9/include/node -I/Users/tjd/.node-gyp/0.12.9/src -I/Users/tjd/.node-gyp/0.12.9/deps/uv/include -I/Users/tjd/.node-gyp/0.12.9/deps/v8/include -I../node_modules/nan  -Os -gdwarf-2 -mmacosx-version-min=10.7 -arch x86_64 -Wall -Wendif-labels -W -Wno-unused-parameter -fno-rtti -fno-exceptions -fno-threadsafe-statics -std=c++11 -stdlib=libc++ -DTARGET_OS_OPENSTEP=1 -MMD -MF ./Release/.deps/Release/obj.target/uDesCrypto/src/UUTF16CStr.o.d.raw   -c -o Release/obj.target/uDesCrypto/src/UUTF16CStr.o ../src/UUTF16CStr.cc
-Release/obj.target/uDesCrypto/src/UUTF16CStr.o: ../src/UUTF16CStr.cc \
-  ../src/UUTF16CStr.h ../src/UnicodeDefinitions.h ../src/KDefinitions.h \
-  ../src/UUTF16Conv.h
-../src/UUTF16CStr.cc:
-../src/UUTF16CStr.h:
-../src/UnicodeDefinitions.h:
-../src/KDefinitions.h:
-../src/UUTF16Conv.h:
